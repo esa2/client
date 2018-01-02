08c39db4b2101629ed95dd4679e45d0aaab1b3a5
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-02 11:07:19 -0800

    Created authenticate function for User and a User constructor.

--- scripts/models/user.js
+++ scripts/models/user.js
@@ -6,5 +6,23 @@ var app = app || {};
   // var __API_URL__ = 'https://uvue.herokuapp.com';
   var __API_URL__ = 'http://localhost:3000';
 
+  // All videos per user interest
+  User.videos = [];
+
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function User(rawUserObj) {
+    Object.keys(rawUserObj).map(key => this[key] = rawUserObj[key]);
+  }
+
+  User.authenticate = (ctx) => {
+    $.get(`${__API_URL__}/api/v1/users/:username`)
+      .then(console.log)
+      .catch(errorCallback);
+  }
+
   module.User = User;
 })(app);
