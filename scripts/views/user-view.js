fe295c52d4259677950085ccc0fd6d775b14ac7e
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-02 13:01:25 -0800

    downloaded pages, added route.js content

--- scripts/views/user-view.js
+++ scripts/views/user-view.js
@@ -3,12 +3,5 @@
 var app = app || {};
 
 (function(module) {
-  bookView.initNewBook = function() {
-    resetView();
-    $('.container').hide()
-    $('#form-view').show()
-    $('#new-book-form').on('change', 'input, textarea', bookView.create);
-    $('#new-book-form').on('submit', bookView.submit);
-  }
   module.userView = userView;
 })(app);
