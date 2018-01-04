c72bb5d4f92c564e426240941a9f02dbf42046e6
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-04 10:13:29 -0800

    got the daily motion videos working BABY!!!!!!

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -6,8 +6,10 @@ page('/signup', app.userView.initSignupView);
 page('/user/:username/feed',
   (ctx, next) => app.User.fetchInterests(ctx, next),
   (ctx, next) => app.userView.initFeedView(ctx, next),
-  (ctx, next) => app.Video.findByInterests(ctx, next),
-  (ctx, next) => app.userView.initVideoList(ctx, next)
+  // (ctx, next) => app.Video.findByInterests(ctx, next),
+  // (ctx, next) => app.userView.initVideoList(ctx, next)
+  (ctx, next) => app.Video.findDmByInterests(ctx, next),
+  (ctx, next) => app.userView.initDmVideoList(ctx, next)
 );
 
 page();
