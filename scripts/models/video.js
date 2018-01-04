b8f502ac594db5a5e67e78586913fc3dee4b2b12
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-04 11:33:33 -0800

    add daily motion feed

--- scripts/models/video.js
+++ scripts/models/video.js
@@ -20,7 +20,7 @@ var __API_URL__ = 'http://localhost:3000';
   }
 
   Video.all = [];
-  Video.findByInterests = (ctx, next) => {
+  Video.findYtByInterests = (ctx, next) => {
     let vids = module.User.interests.map(interest => $.get(`${__API_URL__}/api/v3/videos/search`, {'search': interest}))
 
     $.when.apply($, vids).then(function() {
@@ -29,10 +29,9 @@ var __API_URL__ = 'http://localhost:3000';
       for (let i = 0; i < arguments.length; i++) {
         arguments[i][2].responseJSON.items.forEach(ele => {
           Video.all.push({
+            'source' : 'youtube',
             'videoId' : ele.id.videoId,
-            'thumbnail' : ele.snippet.thumbnails.default,
-            'title' : ele.snippet.title,
-            'publishedDate' : ele.snippet.publishedAt
+            'title' : ele.snippet.title
           });
         })
       }
@@ -40,41 +39,25 @@ var __API_URL__ = 'http://localhost:3000';
       .then(next)
       .catch(errorCallback);
   }
-//dailymotion api 
-  Video.allDm = [];
-  Video.loadAllDm = function() {
-    // Concatenate all videos to the Video.all array
-    Video.allDm = Video.allDm.concat(Video.rawDm.map(ele => {
-      return {
-        'videoId' : ele.id,
-        // 'thumbnail' : ele.snippet.thumbnails.default,
-        'title' : ele.title}
-        // 'publishedDate' : ele.snippet.publishedAt}
-    }))
-    console.log(Video.allDm)
+
+  Video.findDmByInterests = (ctx, next) => {
+    let vids = module.User.interests.map(interest => $.get(`${__API_URL__}/api/dailymotion/videos/search`, { 'search': interest }))
+
+    $.when.apply($, vids).then(function() {
+      console.log(arguments)
+      for (let i = 0; i < arguments.length; i++) {
+        arguments[i][0].list.forEach(ele => {
+          Video.all.push({
+            'source' : 'dailymotion',
+            'videoId' : ele.id,
+            'title' : ele.title
+          });
+        })
+      }
+    })
+      .then(next)
+      .catch(errorCallback);
   }
 
-// api dailymotion stuff
-  Video.findDmByInterests = (ctx, next) => {
-    Video.allDm = [];
-    console.log('Video.findByInterests');
-    module.User.interests.forEach((interest, idx, arr) => {
-      console.log(`Searching for ${interest}`)
-      $.get(`${__API_URL__}/api/dailymotion/videos/search`, { 'search': interest })
-        .then(results => {
-          console.log(results)
-          // console.log(results.items[0].id)
-          Video.rawDm = results.list;
-        })
-        .then(Video.loadAllDm)
-        .then(() => {
-          if (idx === arr.length - 1) {
-            console.log(`CALLING NEXT() idx: ${idx}`);
-            next();
-          }
-        })
-        .catch(errorCallback)
-    });
-  }
   module.Video = Video;
 })(app)
