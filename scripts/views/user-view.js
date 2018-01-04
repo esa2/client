f566dc6d5c417e670806d8e695c0691d3d42022d
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-01-04 15:31:37 -0800

    fixed button animation, text-fit, and sign-up link

--- scripts/views/user-view.js
+++ scripts/views/user-view.js
@@ -12,12 +12,7 @@ var app = app || {};
   // Show the Video Feed
   userView.initFeedView = (ctx, next) => {
     resetView();
-<<<<<<< HEAD
-    $('.logout-div').show();
-    // $('.search-view').show(); COMMENTED OUT IN INDEX.HTML
-=======
     $('.logout-section').show();
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     $('.logout-btn').one('click', function(event){
       event.preventDefault();
       // Remove logged in user for localstorage
@@ -35,13 +30,6 @@ var app = app || {};
       return;
     }
 
-<<<<<<< HEAD
-    console.log('Adding videos now!');
-
-    $('.video-view').show()
-
-=======
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     // Append all videos to the view
     $('.video-view').show()
     app.Video.all.forEach(video => {
@@ -52,34 +40,18 @@ var app = app || {};
         template = Handlebars.compile($('.video-dmlist-template').text());
       }
       $('.video-list').append(template(video));
-<<<<<<< HEAD
-
-=======
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     })
   };
 
   // Show the Signin view
   userView.initSigninView = () => {
     resetView();
-<<<<<<< HEAD
-=======
     $('.signin-section').show();
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
 
     // Clear out the current signin fields
     $('.signin-form input[name="username"]').val('');
     $('.signin-form input[name="password"]').val('');
 
-<<<<<<< HEAD
-    // Show the signin form
-    $('.signin-section').show();
-    $('.logout-btn').hide();
-    // $('.search-btn').hide(); COMMENTED OUT IN INDEX.HTML
-
-
-=======
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     // Set a signin event handler on the signin button once
     $('.signin-form').one('submit', function(e) {
       e.preventDefault();
@@ -92,12 +64,9 @@ var app = app || {};
   userView.initSignupView = () => {
     resetView();
 
-<<<<<<< HEAD
-=======
     // Show the signup form
     $('.signup-section').show();
 
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     // Clear out the current signup fields
     $('.signup-form input[name="username"]').val('');
     $('.signup-form input[name="password"]').val('');
@@ -123,13 +92,6 @@ var app = app || {};
   };
 
   userView.initIndexPage = () => {
-<<<<<<< HEAD
-    $('.signin-btn').hide();
-    $('.signup-btn').hide();
-    $('.signin-form').hide();
-
-=======
->>>>>>> 618d48c633a7eb4790457672c4314a40e768e41f
     // If a user is logged in already immediately navigate to /feed
     if (localStorage.uvueUser) {
       console.log(`Found a logged in user ${localStorage.uvueUser}`)
