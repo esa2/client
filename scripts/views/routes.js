d432049822ab7c4da6ea5ad64a499c62cd87f499
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-05 20:48:54 -0800

    cleanup

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,17 +1,17 @@
-'use strict';
+'use strict'
 
 page.base('/client')
 
-page('/', app.userView.initIndexPage);
-page('/signin', app.userView.initSigninView);
-page('/signup', app.userView.initSignupView);
-// page('/about', app.userView.initAboutPage);
+page('/', app.userView.initIndexPage)
+page('/signin', app.userView.initSigninView)
+page('/signup', app.userView.initSignupView)
+// page('/about', app.userView.initAboutPage)
 page('/user/:username/feed',
   (ctx, next) => app.User.fetchInterests(ctx, next),
   (ctx, next) => app.userView.initFeedView(ctx, next),
   // (ctx, next) => app.Video.findYtByInterests(ctx, next),
   (ctx, next) => app.Video.findDmByInterests(ctx, next),
   (ctx, next) => app.userView.initVideoList(ctx, next)
-);
+)
 
-page();
+page()
--- scripts/views/user-view.js
+++ scripts/views/user-view.js
@@ -1,175 +1,145 @@
-'use strict';
+'use strict'
 
-var app = app || {};
+var app = app || {}
 
-(function(module) {
-  const userView = {};
+;(function(module) {
+  const userView = {}
 
   function resetView() {
-    $('.content').hide();
+    $('.content').hide()
   }
 
   userView.initIndexPage = () => {
-    // If a user is logged in already immediately navigate to /feed
     if (localStorage.uvueUser) {
-      console.log(`Found a logged in user ${localStorage.uvueUser}`)
-      // If their was a user logged in previously, try to fetch the user
       module.User.fetch(JSON.parse(localStorage.uvueUser), null,
-        () => page(`/client/user/${JSON.parse(localStorage.uvueUser)}/feed`));
+        () => page(`/client/user/${JSON.parse(localStorage.uvueUser)}/feed`))
     } else {
-      page('/client/signin');
+      page('/client/signin')
     }
-  };
+  }
 
-  // Show the Signin view
   userView.initSigninView = () => {
-    resetView();
-    // animation hide/show start/stop
-    $(document).ajaxStart(function(){
-      $("#wait").css("display", "block");
-    });
-    $(document).ajaxComplete(function(){
-      $(".hide-anim").hide();
-    });
-
-    // animation hide/show start/stop
-    $('.hide-anim').hide();
-    $('.logout-btn').hide();
-    $('.nav-button').show();
-
-    $('.signin-section').show();
-    // Clear out the current signin fields
-    $('.signin-form input[name="username"]').val('');
-    $('.signin-form input[name="password"]').val('');
-
-    // Set a signin event handler on the signin button once
+    resetView()
+    $('.logout-btn').hide()
+    $('.nav-button').show()
+    $('.signin-section').show()
+    $('.signin-form input[name="username"]').val('')
+    $('.signin-form input[name="password"]').val('')
     $('.signin-form').on('submit', function(e) {
-      e.preventDefault();
-      let username = e.target.username.value;
-      let password = e.target.password.value;
-      module.User.fetch(username, password, module.User.validate);
-    });
-  };
+      e.preventDefault()
+      let username = e.target.username.value
+      let password = e.target.password.value
+      module.User.fetch(username, password, module.User.validate)
+    })
+  }
 
   userView.initSignupView = () => {
-    resetView();
-
-    // Show the signup section
-    $('.signup-section').show();
-
-    // Clear out the current signup fields
-    $('.signup-form input[name="username"]').val('');
-    $('.signup-form input[name="password"]').val('');
-
-    // Set a signup event handler on the signup button once
+    resetView()
+    $('.signup-section').show()
+    $('.signup-form input[name="username"]').val('')
+    $('.signup-form input[name="password"]').val('')
     $('.signup-form').on('submit', function(e) {
-      e.preventDefault();
-      let realname = e.target.realname.value;
-      let username = e.target.username.value;
-      let password = e.target.password.value;
+      e.preventDefault()
+      let realname = e.target.realname.value
+      let username = e.target.username.value
+      let password = e.target.password.value
       module.User.create(username, {
         'realname': realname,
         'password': password,
       })
-    });
-  };
+    })
+  }
 
   // Show the Video Feed
   userView.initFeedView = (ctx, next) => {
-    resetView();
-
-    $('.nav-button').show();
-    $('.logout-btn').show();
+    resetView()
+    $('.nav-button').show()
+    $('.logout-btn').show()
     $('.logout-btn').on('click', function(event){
-      event.preventDefault();
-      // Remove logged in user for localstorage
+      event.preventDefault()
       module.User.logout()
-      page('/client');
-    });
-
+      page('/client')
+    })
     // Prep the interest section
-    $('.add-interest-section').show();
-    userView.initAddInterestSection();
-    next();
-  };
+    $('.add-interest-section').show()
+    userView.initAddInterestSection()
+    next()
+  }
 
   // Show video list
   userView.initVideoList = () => {
-    $('.video-list').empty();
-    // If no videos were found, the user needs to add more interests
+    $('.video-list').empty()
     if (app.Video.all.length === 0) {
-      $('.no-interest-msg').show();
-      return;
+      $('.no-interest-msg').show()
+      return
     }
 
     // Append all videos to the view
     $('.video-view').show()
     app.Video.all.forEach(video => {
-      let template;
+      let template
       if (video.source === 'youtube') {
-        template = Handlebars.compile($('.video-list-template').text());
+        template = Handlebars.compile($('.video-list-template').text())
       } else {
-        template = Handlebars.compile($('.video-dmlist-template').text());
+        template = Handlebars.compile($('.video-dmlist-template').text())
       }
-      $('.video-list').append(template(video));
+      $('.video-list').append(template(video))
     })
-  };
+  }
 
   userView.initAddInterestSection = () => {
     // First, empty the tag editor
-    $('.tag-editor').empty();
+    $('.tag-editor').empty()
     // Populate interest tag editor with current interests
     app.User.interests.map(ele => {
-      let template = Handlebars.compile($('.interest-tag-template').text());
-      $('.tag-editor').append(template({'value': ele}));
+      let template = Handlebars.compile($('.interest-tag-template').text())
+      $('.tag-editor').append(template({'value': ele}))
     })
 
     // Event handler for adding a tag
     $('.interest-add-btn').on('click', function (e) {
-      e.preventDefault();
-      let value = $('#interest-input').val();
-      let template = Handlebars.compile($('.interest-tag-template').text());
-
+      e.preventDefault()
+      let value = $('#interest-input').val()
+      let template = Handlebars.compile($('.interest-tag-template').text())
       // either a duplicate or empty string - disallowed
       if (app.User.interests.indexOf(value) !== -1 || value === '') {
-        return;
+        return
       }
-
       // empty out the input box
-      $('#interest-input').val('');
+      $('#interest-input').val('')
       // Append a new tag to the tag editor box
-      $('.tag-editor').append(template({'value': value}));
+      $('.tag-editor').append(template({'value': value}))
       // Add the tag to the interests array
-      app.User.interests.push(value);
+      app.User.interests.push(value)
       // Add interest to the db
-      app.User.addInterest(value);
-    });
+      app.User.addInterest(value)
+    })
 
     // Event handler for removing a tag
     $('.add-interest-section').on('click', '.cross', function(e) {
-      e.preventDefault();
-      let interest = $(this).parent('a').attr('data-interest');
+      e.preventDefault()
+      let interest = $(this).parent('a').attr('data-interest')
       // Extract the index of the tag to remove
-      let idx = app.User.interests.indexOf(interest);
+      let idx = app.User.interests.indexOf(interest)
       // do it
-      app.User.interests.splice(idx, 1);
-      $(this).parent('a').remove();
+      app.User.interests.splice(idx, 1)
+      $(this).parent('a').remove()
       // Remove interest from the db
-      app.User.removeInterest(interest);
-    });
+      app.User.removeInterest(interest)
+    })
   }
 
   userView.initAboutPage = function () {
-    resetView();
-    $('.about_us-section').show();
-    $('.nav-button').show();
-  };
+    resetView()
+    $('.about_us-section').show()
+    $('.nav-button').show()
+  }
 
   $(document).ready(function(){
     $('.toggle').on('click', function(){
-      $('nav ul').toggleClass('show');
-    });
-  });
+      $('nav ul').toggleClass('show')
+    })
+  })
 
-  module.userView = userView;
-})(app);
+  module.userView = userView
+})(app)
