79bcf7ff03d43b5258d37e94c93e8a9d7ae7aef5
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-01-05 20:18:11 -0800

    reset

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -5,11 +5,11 @@ page.base('/client')
 page('/', app.userView.initIndexPage);
 page('/signin', app.userView.initSigninView);
 page('/signup', app.userView.initSignupView);
-page('/about', app.userView.initAboutPage);
+// page('/about', app.userView.initAboutPage);
 page('/user/:username/feed',
   (ctx, next) => app.User.fetchInterests(ctx, next),
   (ctx, next) => app.userView.initFeedView(ctx, next),
-  (ctx, next) => app.Video.findYtByInterests(ctx, next),
+  // (ctx, next) => app.Video.findYtByInterests(ctx, next),
   (ctx, next) => app.Video.findDmByInterests(ctx, next),
   (ctx, next) => app.userView.initVideoList(ctx, next)
 );
--- scripts/views/user-view.js
+++ scripts/views/user-view.js
@@ -9,100 +9,16 @@ var app = app || {};
     $('.content').hide();
   }
 
-  userView.initAddInterestSection = () => {
-    // First, empty the tag editor
-    $('.tag-editor').empty();
-    // Populate interest tag editor with current interests
-    app.User.interests.map(ele => {
-      let template = Handlebars.compile($('.interest-tag-template').text());
-      $('.tag-editor').append(template({'value': ele}));
-    })
-
-    // Event handler for adding a tag
-    $('.interest-add-btn').on('click', function (e) {
-      e.preventDefault();
-      let value = $('#interest-input').val();
-      let template = Handlebars.compile($('.interest-tag-template').text());
-
-      // either a duplicate or empty string - disallowed
-      if (app.User.interests.indexOf(value) !== -1 || value === '') {
-        return;
-      }
-
-      // empty out the input box
-      $('#interest-input').val('');
-      // Append a new tag to the tag editor box
-      $('.tag-editor').append(template({'value': value}));
-      // Add the tag to the interests array
-      app.User.interests.push(value);
-      // Add interest to the db
-      app.User.addInterest(value);
-    });
-
-    // Event handler for removing a tag
-    $('.add-interest-section').on('click', '.cross', function(e) {
-      e.preventDefault();
-      let interest = $(this).parent('a').attr('data-interest');
-      // Extract the index of the tag to remove
-      let idx = app.User.interests.indexOf(interest);
-      // do it
-      app.User.interests.splice(idx, 1);
-      $(this).parent('a').remove();
-      // Remove interest from the db
-      app.User.removeInterest(interest);
-    });
-  }
-
-  // Show the Video Feed
-  userView.initFeedView = (ctx, next) => {
-    resetView();
-
-    // animation hide/show start/stop
-    $(document).ajaxStart(function(){
-      $("#wait").css("display", "block");
-    });
-    $(document).ajaxComplete(function(){
-      $(".giphy-embed").css("display", "none");
-      $(".hide-anim").hide();
-    });
-    // animation hide/show start/stop
-
-
-    $('.nav-button').show();
-    $('.logout-btn').show();
-    $('.logout-btn').on('click', function(event){
-      event.preventDefault();
-      // Remove logged in user for localstorage
-      module.User.logout()
-      page('/client');
-    });
-
-    // Prep the interest section
-    $('.add-interest-section').show();
-    userView.initAddInterestSection();
-    next();
-  };
-
-  // Show video list
-  userView.initVideoList = () => {
-    $('.video-list').empty();
-    // If no videos were found, the user needs to add more interests
-    if (app.Video.all.length === 0) {
-      $('.no-interest-msg').show();
-      return;
+  userView.initIndexPage = () => {
+    // If a user is logged in already immediately navigate to /feed
+    if (localStorage.uvueUser) {
+      console.log(`Found a logged in user ${localStorage.uvueUser}`)
+      // If their was a user logged in previously, try to fetch the user
+      module.User.fetch(JSON.parse(localStorage.uvueUser), null,
+        () => page(`/client/user/${JSON.parse(localStorage.uvueUser)}/feed`));
+    } else {
+      page('/client/signin');
     }
-
-    // Append all videos to the view
-    $('.video-view').show()
-    app.Video.all.forEach(video => {
-      let template;
-      if (video.source === 'youtube') {
-        template = Handlebars.compile($('.video-list-template').text());
-      } else {
-        template = Handlebars.compile($('.video-dmlist-template').text());
-      }
-      $('.video-list').append(template(video));
-    })
   };
 
   // Show the Signin view
@@ -158,17 +74,90 @@ var app = app || {};
     });
   };
 
-  userView.initIndexPage = () => {
-    // If a user is logged in already immediately navigate to /feed
-    if (localStorage.uvueUser) {
-      console.log(`Found a logged in user ${localStorage.uvueUser}`)
-      // If their was a user logged in previously, try to fetch the user
-      module.User.fetch(JSON.parse(localStorage.uvueUser), null,
-        () => page(`/client/user/${JSON.parse(localStorage.uvueUser)}/feed`));
-    } else {
-      page('/client/signin');
-    }
+  // Show the Video Feed
+  userView.initFeedView = (ctx, next) => {
+    resetView();
+
+    $('.nav-button').show();
+    $('.logout-btn').show();
+    $('.logout-btn').on('click', function(event){
+      event.preventDefault();
+      // Remove logged in user for localstorage
+      module.User.logout()
+      page('/client');
+    });
+
+    // Prep the interest section
+    $('.add-interest-section').show();
+    userView.initAddInterestSection();
+    next();
   };
+
+  // Show video list
+  userView.initVideoList = () => {
+    $('.video-list').empty();
+    // If no videos were found, the user needs to add more interests
+    if (app.Video.all.length === 0) {
+      $('.no-interest-msg').show();
+      return;
+    }
+
+    // Append all videos to the view
+    $('.video-view').show()
+    app.Video.all.forEach(video => {
+      let template;
+      if (video.source === 'youtube') {
+        template = Handlebars.compile($('.video-list-template').text());
+      } else {
+        template = Handlebars.compile($('.video-dmlist-template').text());
+      }
+      $('.video-list').append(template(video));
+    })
+  };
+
+  userView.initAddInterestSection = () => {
+    // First, empty the tag editor
+    $('.tag-editor').empty();
+    // Populate interest tag editor with current interests
+    app.User.interests.map(ele => {
+      let template = Handlebars.compile($('.interest-tag-template').text());
+      $('.tag-editor').append(template({'value': ele}));
+    })
+
+    // Event handler for adding a tag
+    $('.interest-add-btn').on('click', function (e) {
+      e.preventDefault();
+      let value = $('#interest-input').val();
+      let template = Handlebars.compile($('.interest-tag-template').text());
+
+      // either a duplicate or empty string - disallowed
+      if (app.User.interests.indexOf(value) !== -1 || value === '') {
+        return;
+      }
+
+      // empty out the input box
+      $('#interest-input').val('');
+      // Append a new tag to the tag editor box
+      $('.tag-editor').append(template({'value': value}));
+      // Add the tag to the interests array
+      app.User.interests.push(value);
+      // Add interest to the db
+      app.User.addInterest(value);
+    });
+
+    // Event handler for removing a tag
+    $('.add-interest-section').on('click', '.cross', function(e) {
+      e.preventDefault();
+      let interest = $(this).parent('a').attr('data-interest');
+      // Extract the index of the tag to remove
+      let idx = app.User.interests.indexOf(interest);
+      // do it
+      app.User.interests.splice(idx, 1);
+      $(this).parent('a').remove();
+      // Remove interest from the db
+      app.User.removeInterest(interest);
+    });
+  }
 
   userView.initAboutPage = function () {
     resetView();
