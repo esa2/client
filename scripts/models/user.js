91e0b5ae73b9ebbe8f545375c090cc05fab8edb2
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-15 10:04:03 -0800

    switch to heroku

--- scripts/models/user.js
+++ scripts/models/user.js
@@ -3,8 +3,8 @@
 var app = app || {}
 
 ;(function(module) {
-  // var __API_URL__ = 'https://uvue.herokuapp.com'
-  var __API_URL__ = 'http://localhost:3000'
+  var __API_URL__ = 'https://uvue-video.herokuapp.com/'
+  //  var __API_URL__ = 'http://localhost:3000'
 
   User.user = null
   User.interests = null
